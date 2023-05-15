4ee1fe78529e7736d74194336e2555b002ed17fb
CruelGs <gustavopamponet963@gmail.com>
2023-05-15 11:33:48 -0300

    up

---
+++ Estudos/4 - Condições/condiçao01.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60)
+{console.log('Você ultrapassou a velocidade permitida! Você foi Multado em 180 Reais.')}
+else
+{console.log('Dirija com Cuidado!')}
---
+++ Estudos/4 - Condições/condiçao02.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+console.log(`Nascido no pais ${pais}`)
+if (pais == 'Brasil')
+{console.log('Você é Brasileiro.')}
+else
+{console.log('Você é Estrangeiro.')}
