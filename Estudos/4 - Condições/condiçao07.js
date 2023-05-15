696ca84745ad7a5cfc619567e58d2021c66efa83
CruelGs <gustavopamponet963@gmail.com>
2023-05-15 17:43:33 -0300

    up

---
+++ Estudos/4 - Condições/condiçao07.js
@@ -0,0 +1,12 @@
+var hora = 00
+if (hora >=6 && hora < 12)
+    {console.log('Bom dia!')}
+
+else if (hora > 11 && hora <= 17)
+    {console.log('Boa tarde!')}
+
+else if (hora > 17 && hora <= 23)
+    {console.log('Boa noite!')}
+
+else
+    {console.log('Boa madrugada!')}
