38a967908541a2904c6ac79ed1878f77ee0682e5
CruelGs <gustavopamponet963@gmail.com>
2023-05-15 14:42:21 -0300

    up

---
+++ Estudos/4 - Condições/condiçao05.js
@@ -0,0 +1,9 @@
+var idade = 14
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16)
+{console.log('Não vota.')}
+else
+    if (idade < 18 || idade >67)
+    {console.log('Voto é opcional.')}
+    else
+    {console.log('O voto é obrigatório.')}
