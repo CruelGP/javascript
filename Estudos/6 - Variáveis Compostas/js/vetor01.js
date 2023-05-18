9106e0cd00cfbb71de5d2f22bce9eb2b0e150497
CruelGs <gustavopamponet963@gmail.com>
2023-05-18 18:45:35 -0300

    up

--- Estudos/6 - Variáveis Compostas/js/vetor01.js
+++ Estudos/6 - Variáveis Compostas/js/vetor01.js
@@ -6,3 +6,6 @@ num.push(6)
     console.log(num)
     console.log(`O vetor tem ${num.length} posições`)
     console.log(`O primeiro valor do vetor pe ${num[0]}`)
+
+for(var pos = 0; pos < num.length; pos++)
+    {console.log(`A posição ${pos} tem o valor ${num[pos]}`)}
