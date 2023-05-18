34047b37f4a0d3886236b9a8c3f84ceae496e843
CruelGs <gustavopamponet963@gmail.com>
2023-05-17 22:03:32 -0300

    up

--- Estudos/5 - Repetições/js/execicio02.js
+++ Estudos/5 - Repetições/js/execicio02.js
@@ -1,7 +1,10 @@
 function run(){
     var c = 1
     var n = window.document.querySelector(`input#n`)
+    var tab = document.querySelector('select#seltab')
     var num = Number(n.value)
         for (num;c <= 10;c++)
-            {res.innerHTML += (`${num} x ${c} = ${num * c}`)}
+            {var item = document.createElement('option')
+             item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)}
 }
