50392dddbfd5db729c0c40f43ab3c9b8d6b48230
CruelGs <gustavopamponet963@gmail.com>
2023-06-18 17:30:50 -0300

    up

---
+++ Estudos/Guia definitivo/Capitulo 01/cao01-arrays.js
@@ -0,0 +1,18 @@
+var prime = [2,3,5,7]
+console.log (prime)
+//Lê o elemento de acordo com a ordem que foi adicionado do 0 ao 3
+console.log (prime[0])
+//lê a quantidade de elementos do array.
+console.log (prime.length)
+// Lê o ultimo elemento do array.
+console.log (prime[prime.length-1])
+//adiciona um novo elemento no array.
+prime[4] = 9
+console.log (prime)
+//Alterao o elemento ja existente por meio de atrbuição.
+prime[4] = 11
+console.log(prime)
+
+var empty = []
+empty[0] = "Loud"
+console.log (empty)
---
+++ Estudos/Guia definitivo/Capitulo 01/cap01-objeto.js
@@ -0,0 +1,9 @@
+var book = {topic:"JavaScript", fat:true};
+
+console.log (book.topic)
+console.log(book['fat'])
+book.author = "Flanagem"
+console.log(book.author)
+book.contents = {}
+
+console.log(book)
